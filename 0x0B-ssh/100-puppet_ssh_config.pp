9a15d2689bf89f293c8905b27a9455a6266797e0
Leonard Nzekwe <nzekweleo@gmail.com>
2023-09-24 14:06:18 -0700

    Update Task File, task-100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 # Using Puppet to create client configuration file
+
+file_line {'Ascertain the config file contains the identity file config':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
+
+file_line {'Ascertain the config file contains the pass_auth set to no':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
